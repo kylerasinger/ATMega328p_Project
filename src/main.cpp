3c4b5cedfd0fbbf37b9b24a7254e2c4daff0a018
kylerasinger <kyle.rasinger@gmail.com>
2024-03-26 14:09:09 -0400

    Added fan Implementation

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 #include "Utilities/I2C.h"
 #include "Timer/TimerTwo.h"
 #include "Timer/TimerZero.h"
-
+#include "Fans/Fan.h"
 
 
 /* ---===       PIN DECLARATION      ===--- */
@@ -27,9 +27,6 @@
   //define pins for the US sensor
   #define US_TRIG PB4
   #define US_ECHO PB0
-
-  //define pins for the IR
-  #define IR_IN PC0
 
   //define LED pin
   #define LED_PIN PB3
@@ -42,25 +39,37 @@
 
   //define Servo pins
   #define SERVO_PIN PB1
+    //uses timer 1 for PWM
+
+  //define lift fan pins
+  #define L_FAN_PIN PD5 //lift
+  #define T_FAN_PIN PD6 //thrust
+    //uses timer 0 for PWM
 
 /* ---===   END OF PIN DECLARATION   ===--- */
 
 /* ---===   RESOURCES   ===--- /
 
-  Timer 0 is for general timing (1 second outputs)
+  Timer 0 is for general timing (1 second outputs), Lift and thrust fan PWM
   Timer 1 is being used by servo for PWM
   Timer 2 is being used for IMU motion tracking, the US Sensor and the LED's brightness.
 
 /  ---===               ===--- */
 
+
 int main(void) {
-  TimerZero timerZero;
+  // TimerZero timerZero;
   TimerTwo timerTwo;
   UART uart;
   UsSensor usSensor(US_ECHO, US_TRIG, &uart);
-  IrSensor irSensor(IR_IN, &uart);
+  Fan liftFan(L_FAN_PIN);
+  Fan thrustFan(T_FAN_PIN);
+
   LED largeLED(LED_PIN);
-  MPU6050 mpu(MPU6050_ADDRESS, 0b00011000, 0b00000000, &largeLED);
+  largeLED.setBrightness(0);
+  largeLED.setBrightness(255);
+
+  MPU6050 mpu(MPU6050_ADDRESS, 0b00000000, 0b00000000, &largeLED);
   Servo servo(SERVO_PIN);
 
   sei(); //enable interrupts
@@ -69,12 +78,10 @@ int main(void) {
   float yawChange;
 
   float velocityX = 0.0;
-  float velocityXChange;
-
-  float distanceX = 0.0;
+  float distanceX = 1000.0;
   float distanceXChange;
 
-  timerZero.start();
+  // timerZero.start();
 
   /*
     Two major questions for the POD session
@@ -83,62 +90,62 @@ int main(void) {
   
   */
 
+  // liftFan.setSpeed(255);
+
   while(true){
-    timerTwo.start();
+    // timerTwo.start();
 
-    mpu.readSensor(&largeLED);
+    // mpu.readSensor(&largeLED);
 
-    //This is all printing for debugging
-      // unsigned long intPart = static_cast<unsigned long>(yaw);
-      // unsigned long fracPart = static_cast<unsigned long>(fabs(yaw - intPart) * 100000);
-      char buffer[25];
-      // sprintf(buffer, "Yaw (degrees): %lu.%05lu", intPart, fracPart);
-      // uart.println(buffer);
+    // //This is all printing for debugging
+    //   // unsigned long intPart = static_cast<unsigned long>(yaw);
+    //   // unsigned long fracPart = static_cast<unsigned long>(fabs(yaw - intPart) * 100000);
+    //   char buffer[25];
+    //   // sprintf(buffer, "Yaw (degrees): %lu.%05lu", intPart, fracPart);
+    //   // uart.println(buffer);
 
-      // unsigned long intPart2 = static_cast<unsigned long>(distanceX);
-      // unsigned long fracPart2 = static_cast<unsigned long>(fabs(distanceX - intPart2) * 100000);
-      // sprintf(buffer, "DistanceX (m): %lu.%05lu", intPart2, fracPart2);
-      // uart.println(buffer);
-    // end of printing for debugging
+    //   unsigned long intPart2 = static_cast<unsigned long>(distanceX);
+    //   unsigned long fracPart2 = static_cast<unsigned long>(fabs(distanceX - intPart2) * 100000);
+    //   sprintf(buffer, "DistanceX (m): %lu.%05lu", intPart2, fracPart2);
+    //   uart.println(buffer);
+    // // end of printing for debugging
 
-    if((int)yaw > 5 && (int)yaw < 175){
-      servo.setServoAngle((int)yaw);
-      turnOffYellowLED();
-    }else{
-      turnOnYellowLED();
-    }
+    // if((int)yaw > 5 && (int)yaw < 175){
+    //   servo.setServoAngle((int)yaw);
+    //   turnOffYellowLED();
+    // }else{
+    //   turnOnYellowLED();
+    // }
 
-    int intAccelX = (int)((mpu.getAccelX_g()) * 100);
-    int absIntAccelX = abs(intAccelX);
+    // int intAccelX = (int)((mpu.getAccelX_g()) * 100);
+    // int absIntAccelX = abs(intAccelX);
 
-    if(absIntAccelX < 8 ){
-      largeLED.setBrightness(0);
-    }else if(absIntAccelX > 108){
-      largeLED.setBrightness(255);
-    }else if(absIntAccelX > 8 && absIntAccelX < 108){
-      int mappedBrightness = map(absIntAccelX, 8, 108, 0, 255);
-      largeLED.setBrightness(mappedBrightness);
-    }
+    // if(absIntAccelX < 8 ){
+    //   largeLED.setBrightness(0);
+    // }else if(absIntAccelX > 108){
+    //   largeLED.setBrightness(255);
+    // }else if(absIntAccelX > 8 && absIntAccelX < 108){
+    //   int mappedBrightness = map(absIntAccelX, 8, 108, 0, 255);
+    //   largeLED.setBrightness(mappedBrightness);
+    // }
 
-    timerTwo.read();
-    timerTwo.stop();
+    // timerTwo.read();
+    // timerTwo.stop();
 
-    yawChange = mpu.getGyroZ_degPerSec() * timerTwo.timeInSeconds;
-    yaw -= yawChange*1.3;
+    // yawChange = mpu.getGyroZ_degPerSec() * timerTwo.timeInSeconds;
+    // yaw -= yawChange*1.3;
     
-    float accelX_ms2 = mpu.getAccelX_g() * 9.81;
-    velocityX += accelX_ms2 * timerTwo.timeInSeconds;
-    distanceXChange = velocityX * timerTwo.timeInSeconds;
-    distanceX += distanceXChange;
+    // float accelX_ms2 = mpu.getAccelX_g() * 9.81;
+    // velocityX += accelX_ms2 * timerTwo.timeInSeconds;
+    // distanceXChange = velocityX * timerTwo.timeInSeconds;
+    // distanceX += distanceXChange;
 
-    timerZero.read();
-    if(timerZero.timeInSeconds > 1.0){
-      mpu.printSensorReadings(&largeLED, &uart);
-      timerZero.stop();
-      timerZero.start();
-    }
-          // mpu.printSensorReadings(&largeLED, &uart);
-
+    // timerZero.read();
+    // if(timerZero.timeInSeconds > 1.0){
+    //   mpu.printSensorReadings(&largeLED, &uart);
+    //   timerZero.stop();
+    //   timerZero.start();
+    // }
   }
 }
 
